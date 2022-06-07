94e9fee6bfdf37e739cef301fe77cb3c38b8fd42
bmleon <belenleon10.98@gmail.com>
2022-06-07 23:46:06 +0200

    docs: documentación javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"examenOptimizacionYDocumentacion","l":"EdadErroneaException"},{"p":"examenOptimizacionYDocumentacion","l":"Perro"}];updateSearchResults();
